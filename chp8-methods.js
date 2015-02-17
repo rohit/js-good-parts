583c90ed6bf0fe507a473da8131da4fd4c8efabb
Rohit Arondekar <rohit.arondekar@gmail.com>
2015-02-17 21:25:07 +0530

    Chapter 8 Methods

---
+++ chp8-methods.js
@@ -0,0 +1,8 @@
+// Chapter 8: Methods
+//
+// This chapter is like a mini reference to some of the methods available on
+// built in objects.
+// Trying them out in the REPL I don't think I should make notes of them since
+// they can be looked up on MDN.
+// The book also provides possible implementations using other primitives like
+// slice, splice and apply.
