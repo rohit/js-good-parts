315c96b53a1acc274a487e9faec0b92cb8744756
Rohit Arondekar <rohit.arondekar@gmail.com>
2015-02-17 21:47:42 +0530

    Chapter 9 Style

---
+++ chp9-style.js
@@ -0,0 +1,7 @@
+// Chapter 9: Style
+// Computer programs are the most complex things humans make? I can understand
+// one of the most complex but _the most_ seems too much.
+// This chapter contains the usual advice on writing good, clear & concise code.
+// I really like adding a space between if and () which is supported by
+// Crockford. Always add semicolons at end of statements to avoid auto addition
+// by interpreter! Use block versions of if, while and for.
