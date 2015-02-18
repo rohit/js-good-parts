7b66a4f3f9da0f7bb3d307aec2f03fc3f9bd07e9
Rohit Arondekar <rohit.arondekar@gmail.com>
2015-02-18 18:04:22 +0530

    Chp 10 Beautiful Features

---
+++ chp10-beautiful-features.js
@@ -0,0 +1,15 @@
+// Chapter 10: Beautiful Features
+// Crockford wrote a chapter in Beautiful Code in which he implemented a top
+// down operator precedence parser. He used a subset of JS and included only
+// parts that were the best of the best.
+// 1. Functions as first class objects
+// 2. Dynamic objects with prototypal inheritance
+// 3. Object and Array literals
+//
+// Simplified JS is not a strict subset as some new features were added.
+//
+// Simplified JS interestingly doesn't have reserved words! In a function a name
+// can be either a var/param or language feature. You can't use both. Sounds
+// simple but are there other mainstream languages which do this?
+//
+// Also added block scope.
